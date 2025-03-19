acbf8f274a47f1176fc7bb8c32d9eb30bbc7fbf0
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2025-03-19 20:52:32 +0100

    Initial commit

---
+++ TodoufukenFlagGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoufukenFlagGame
+//
+//  Created by Serrano Soria, Juan on 19/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
