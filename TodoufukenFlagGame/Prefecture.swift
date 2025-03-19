8ea233b9195d31624d01a5986db678e4260c7603
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2025-03-19 20:55:20 +0100

    Add flag images and Prefecture struct

---
+++ TodoufukenFlagGame/Prefecture.swift
@@ -0,0 +1,62 @@
+//
+//  Prefecture.swift
+//  TodoufukenFlagGame
+//
+//  Created by Serrano Soria, Juan on 19/03/2025.
+//
+
+// Prefecture.swift
+struct Prefecture {
+    let romaji: String
+    let kanji: String
+    
+    static let all: [Prefecture] = [
+        Prefecture(romaji: "aichi", kanji: "愛知県"),
+        Prefecture(romaji: "akita", kanji: "秋田県"),
+        Prefecture(romaji: "aomori", kanji: "青森県"),
+        Prefecture(romaji: "chiba", kanji: "千葉県"),
+        Prefecture(romaji: "ehime", kanji: "愛媛県"),
+        Prefecture(romaji: "fukui", kanji: "福井県"),
+        Prefecture(romaji: "fukuoka", kanji: "福岡県"),
+        Prefecture(romaji: "fukushima", kanji: "福島県"),
+        Prefecture(romaji: "gifu", kanji: "岐阜県"),
+        Prefecture(romaji: "gunma", kanji: "群馬県"),
+        Prefecture(romaji: "hiroshima", kanji: "広島県"),
+        Prefecture(romaji: "hokkaido", kanji: "北海道"), // Special: 道 (dō)
+        Prefecture(romaji: "hyogo", kanji: "兵庫県"),
+        Prefecture(romaji: "ibaraki", kanji: "茨城県"),
+        Prefecture(romaji: "ishikawa", kanji: "石川県"),
+        Prefecture(romaji: "iwate", kanji: "岩手県"),
+        Prefecture(romaji: "kagawa", kanji: "香川県"),
+        Prefecture(romaji: "kagoshima", kanji: "鹿児島県"),
+        Prefecture(romaji: "kanagawa", kanji: "神奈川県"),
+        Prefecture(romaji: "kochi", kanji: "高知県"),
+        Prefecture(romaji: "kumamoto", kanji: "熊本県"),
+        Prefecture(romaji: "kyoto", kanji: "京都府"), // Special: 府 (fu)
+        Prefecture(romaji: "mie", kanji: "三重県"),
+        Prefecture(romaji: "miyagi", kanji: "宮城県"),
+        Prefecture(romaji: "miyazaki", kanji: "宮崎県"),
+        Prefecture(romaji: "nagano", kanji: "長野県"),
+        Prefecture(romaji: "nagasaki", kanji: "長崎県"),
+        Prefecture(romaji: "nara", kanji: "奈良県"),
+        Prefecture(romaji: "niigata", kanji: "新潟県"),
+        Prefecture(romaji: "oita", kanji: "大分県"),
+        Prefecture(romaji: "okayama", kanji: "岡山県"),
+        Prefecture(romaji: "okinawa", kanji: "沖縄県"),
+        Prefecture(romaji: "osaka", kanji: "大阪府"), // Special: 府 (fu)
+        Prefecture(romaji: "saga", kanji: "佐賀県"),
+        Prefecture(romaji: "saitama", kanji: "埼玉県"),
+        Prefecture(romaji: "shiga", kanji: "滋賀県"),
+        Prefecture(romaji: "shimane", kanji: "島根県"),
+        Prefecture(romaji: "shizuoka", kanji: "静岡県"),
+        Prefecture(romaji: "tochigi", kanji: "栃木県"),
+        Prefecture(romaji: "tokushima", kanji: "徳島県"),
+        Prefecture(romaji: "tokyo", kanji: "東京都"), // Special: 都 (to)
+        Prefecture(romaji: "tottori", kanji: "鳥取県"),
+        Prefecture(romaji: "toyama", kanji: "富山県"),
+        Prefecture(romaji: "wakayama", kanji: "和歌山県"),
+        Prefecture(romaji: "yamagata", kanji: "山形県"),
+        Prefecture(romaji: "yamaguchi", kanji: "山口県"),
+        Prefecture(romaji: "yamanashi", kanji: "山梨県")
+    ]
+}
