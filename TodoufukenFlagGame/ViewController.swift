e284eb610ffb1f25e32460e886fe039ee5ad9e56
Juan Serrano Soria <54719300+juan-serrano-soria@users.noreply.github.com>
2025-03-20 21:56:32 +0100

    Randomize flags shown and target answer

--- TodoufukenFlagGame/ViewController.swift
+++ TodoufukenFlagGame/ViewController.swift
@@ -11,6 +11,10 @@ class ViewController: UIViewController {
     @IBOutlet var flagButton1: UIButton!
     @IBOutlet var flagButton2: UIButton!
     @IBOutlet var flagButton3: UIButton!
+    
+    var prefectures = Prefecture.all
+    var score = 0
+    var correctAnswer = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +26,20 @@ class ViewController: UIViewController {
         flagButton1.layer.borderColor = UIColor.lightGray.cgColor
         flagButton2.layer.borderColor = UIColor.lightGray.cgColor
         flagButton3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
 
+    func askQuestion() {
+        prefectures.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        flagButton1.setImage(UIImage(named: prefectures[0].romaji), for: .normal)
+        flagButton2.setImage(UIImage(named: prefectures[1].romaji), for: .normal)
+        flagButton3.setImage(UIImage(named: prefectures[2].romaji), for: .normal)
+        
+        title = prefectures[correctAnswer].kanji
+    }
 
 }
 
